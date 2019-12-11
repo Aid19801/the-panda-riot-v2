4980d37571748b4abf6850c35728c1060ee9a182
Aid Thompson <thePandaRiot@gmail.com>
2019-12-11 18:31:55 +0000

    routing on ppl who played here

--- components/PeopleWhoPlayedHere/index.js
+++ components/PeopleWhoPlayedHere/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import Link from 'next/link'
 import './styles.css';
 
 function PeopleWhoPlayedHere({ people }) {
     return (
         <div className="gigs__more-info-container container tpr__border flex-center padding-on">
             <p>Who's Played Here</p>
-        <div className="selectedGig__ppl-played-here w-100 row">
-            {people && people.length !== 0 && people.map((each, i) => {
-                return <img key={i} src={each.profilePicture} alt="person who played at gig" />
-            })}
-        </div>
+            <div className="selectedGig__ppl-played-here w-100 row">
+                {people && people.length !== 0 && people.map((each, i) => {
+                    return (
+                        <>
+                            <Link href={`/acts/${each.uid}`}>
+                                <a>
+                                    <img key={i} src={each.profilePicture} alt="person who played at gig" />
+                                </a>
+                            </Link>
+                        </>
+                    )
+                })}
+            </div>
         </div>
     )
 }
